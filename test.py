33be0fa32d97d93f7cda506c1ff1253024539f0d
xAlexMGGx <alex.m.guinea@gmail.com>
2023-11-20 18:57:51 +0100

    Images to calibrate

---
+++ test.py
@@ -0,0 +1,30 @@
+import cv2
+from picamera2 import Picamera2
+from pyglet.window import key
+from time import perf_counter
+
+def stream_video():
+    picam = Picamera2()
+    picam.preview_configuration.main.size=(640, 360)
+    picam.preview_configuration.main.format="RGB888"
+    picam.preview_configuration.align()
+    picam.configure("preview")
+    picam.start()
+    #keys = key.KeyStateHandler()
+    idx = 0
+    t1 = perf_counter()
+    while True:
+        frame = picam.capture_array()
+        cv2.imshow("picam", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        #if keys[key.SPACE]:
+        if perf_counter()-t1 > 5:
+            cv2.imwrite(f'img{idx}.jpg', frame)
+            idx += 1
+            t1 = perf_counter()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    
+    stream_video()
