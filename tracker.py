4df77f60ecd586e25487baa39633dde8f5783d72
xAlexMGGx <alex.m.guinea@gmail.com>
2023-11-29 12:01:10 +0100

    Tracker de esfera amarilla

--- tracker.py
+++ tracker.py
@@ -1,90 +1,111 @@
 import cv2
-from picamera2 import Picamera2
-from pyglet.window import key
-from time import perf_counter
-from collections import deque
 import numpy as np
-import argparse
-import imutils  
+from PIL import Image
+from picamera2 import Picamera2
+
+
+# Function to check if a specific pattern is present in the frame
+def check_pattern(frame, pattern):
+    # Convert the pattern to grayscale
+    pattern_gray = cv2.cvtColor(pattern, cv2.COLOR_BGR2GRAY)
+
+    # Create SIFT object
+    sift = cv2.SIFT_create()
+
+    # Detect keypoints and compute descriptors for the pattern
+    keypoints_pattern, descriptors_pattern = sift.detectAndCompute(pattern_gray, None)
+
+    # Convert the frame to grayscale
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect keypoints and compute descriptors for the frame
+    keypoints_frame, descriptors_frame = sift.detectAndCompute(frame_gray, None)
+
+    # Create a BFMatcher object
+    bf = cv2.BFMatcher()
+
+    # Match descriptors of the pattern and frame
+    matches = bf.match(descriptors_pattern, descriptors_frame)
+
+    # Sort the matches by distance
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    # Draw the top matches on the frame
+    matched_frame = cv2.drawMatches(pattern_gray, keypoints_pattern, frame_gray, keypoints_frame, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    # Show the matched frame
+    cv2.imshow('Pattern Matching', matched_frame)
+
+    # If pattern is found, print 'hello'
+    if len(matches) > 0:
+        print('hello')
 
 
 def hsv_to_range(hsv):
     # blue = [219*179//360, 0.93*255, 0.41*255]
     hsv = [hsv[0]*179//360, hsv[1]/100*255, hsv[2]/100*255]
-    return np.array([hsv[0] - 10, hsv[1] - 50, hsv[2] - 50]), np.array([hsv[0] + 10, hsv[1] + 50, hsv[2] + 50])
+    return np.array([hsv[0] - 30, hsv[1] - 150, hsv[2] - 150]), np.array([hsv[0] + 30, hsv[1] + 150, hsv[2] + 150])
 
 
-def stream_video():
-    picam = Picamera2()
-    picam.preview_configuration.main.size=(320, 180)
-    picam.preview_configuration.main.format="RGB888"
-    picam.preview_configuration.align()
-    picam.configure("preview")
-    picam.start()
-    #keys = key.KeyStateHandler()
-    idx = 0
-    t1 = perf_counter()
-    orange = (28,100,100)
-    orange_low, orange_up = hsv_to_range(orange)
-    pts = deque(maxlen=64)
-    while True:
-        frame = picam.capture_array()
-        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-        # construct a mask for the color "green", then perform
-        # a series of dilations and erosions to remove any small
-        # blobs left in the mask
-        mask = cv2.inRange(hsv, orange_low, orange_up)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        # find contours in the mask and initialize the current
-        # (x, y) center of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        center = None
-        # only proceed if at least one contour was found
-        if len(cnts) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
-            c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+# Pattern to be matched
+pattern = cv2.imread('path_to_pattern_image')
 
-            # #############################
-            # ESTO A LO MEJOR HAY QUE CAMBIARLO DEPENDIENDO DE LA PELOTA
-            # #############################
+# Yellow filter
+yellow = [66, 100, 100]
+yellow_low, yellow_up = hsv_to_range(yellow)
 
-            # only proceed if the radius meets a minimum size
-            if radius > 10:
+# Open video capture
+picam = Picamera2()
+picam.preview_configuration.main.size=(1280, 720)
+picam.preview_configuration.main.format="RGB888"
+picam.preview_configuration.align()
+picam.configure("preview")
+picam.start()
 
 
+while True:
+    # Read the next frame
+    frame = picam.capture_array()
 
-                # draw the circle and centroid on the frame,
-                # then update the list of tracked points
-                cv2.circle(frame, (int(x), int(y)), int(radius),
-                    (0, 255, 255), 2)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
-        # update the points queue
-        pts.appendleft(center)
+    # Convert frame to hsv
+    hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # loop over the set of tracked points
-        for i in range(1, len(pts)):
-            # if either of the tracked points are None, ignore
-            # them
-            if pts[i - 1] is None or pts[i] is None:
-                continue
-            # otherwise, compute the thickness of the line and
-            # draw the connecting lines
-            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-        # show the frame to our screen
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-        # if the 'q' key is pressed, stop the loop
+    # Create a mask for the yellow color
+    mask = cv2.inRange(frame, yellow_low, yellow_up)
 
-if __name__ == "__main__":
-    
-    stream_video()
+    # Blur the mask
+    mask = cv2.GaussianBlur(mask, (11, 11), 0)
+
+    # Apply a threshold to the frame
+    _, threshold = cv2.threshold(mask, 50, 255, cv2.THRESH_BINARY)
+
+    # Find contours of the thresholded frame
+    contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for contour in contours:
+        # Area of contour
+        area = cv2.contourArea(contour)
+        if area < 1000:
+            continue
+        
+        # Approximate the contour
+        approx_poly = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
+        if len(approx_poly) < 10:
+            continue
+
+        # Draw bounding rectangle around the contour
+        x, y, w, h = cv2.boundingRect(approx_poly)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Show the frame with bounding rectangles
+    cv2.imshow('Motion Detection', frame)
+
+    # Perform pattern matching
+    # check_pattern(frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cv2.destroyAllWindows()
